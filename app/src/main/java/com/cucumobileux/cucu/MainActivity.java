318445eba19d12d42f0ea3c72165d3c18402d180
GeraldineGomez <gs.gomezm1@uniandes.edu.co>
2024-09-26 19:43:14 -0500

    feat(David): create login view

--- app/src/main/java/com/cucumobileux/cucu/MainActivity.java
+++ app/src/main/java/com/cucumobileux/cucu/MainActivity.java
@@ -1,6 +1,11 @@
 package com.cucumobileux.cucu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,39 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Find the ImageView by ID
+        ImageView logo = findViewById(R.id.logo);
+
+        // Set an OnClickListener on the ImageView
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirect to SecondActivity when the image is clicked
+                Intent intent = new Intent(MainActivity.this, QRActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imgQr = findViewById(R.id.qrCode);
+        imgQr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirect to SecondActivity when the image is clicked
+                Intent intent = new Intent(MainActivity.this, QRActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView textQr = findViewById(R.id.qrText);
+        textQr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirect to SecondActivity when the image is clicked
+                Intent intent = new Intent(MainActivity.this, QRActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
